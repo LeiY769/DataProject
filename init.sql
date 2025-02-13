9821b344fd73882314cb998af8fc5f93d2cbca33
LordAce <sayleesx@gmail.com>
2025-02-14 00:13:56 +0100

    Database utility

---
+++ init.sql
@@ -0,0 +1,108 @@
+START TRANSACTION;
+
+-- For TRAIN_SERV
+CREATE TABLE SERVICE (
+    id INT PRIMARY KEY, -- id of the service, autoincrement
+    name VARCHAR(100) NOT NULL -- name of the service
+) ;
+
+-- For UNIQUE_STATION
+CREATE TABLE STATIONS (
+    id INT PRIMARY KEY, -- id of the station, autoincrement
+    name VARCHAR(255) NOT NULL, -- name of the station
+    region VARCHAR(255) NOT NULL, -- region of the station
+    latitude DOUBLE PRECISION NOT NULL, -- latitude of the station
+    longitude DOUBLE PRECISION NOT NULL -- longitude of the station
+) ;
+
+CREATE TABLE DATANASE (
+    departure_date DATE,
+    train_number INT,
+    relation INT,
+    train_service INT,
+    ptcar_number INT,
+    line_number_departure VARCHAR(255),
+    real_time_arrival TIME,
+    real_time_departure TIME,
+    planned_time_arrival TIME,
+    planned_time_departure TIME,
+    delay_arrival INT,
+    delay_departure INT,
+    ptcar_name INT,
+    line_number_arrival VARCHAR(255),
+    station_departure INT,
+    station_arrival INT,
+
+    FOREIGN KEY (train_service) REFERENCES SERVICE(id),
+    FOREIGN KEY (ptcar_name) REFERENCES STATIONS(id),
+    FOREIGN KEY (station_departure) REFERENCES STATIONS(id),
+    FOREIGN KEY (station_arrival) REFERENCES STATIONS(id)
+) ;
+
+CREATE TABLE WALLONIA_WEATHER (
+    temperature_2m REAL NOT NULL,
+    apparent_temperature REAL NOT NULL,
+    dewpoint_2m REAL NOT NULL,  
+    relative_humidity_2m REAL NOT NULL,
+    precipitation REAL NOT NULL,
+    rain REAL NOT NULL,
+    snowfall  REAL NULL,
+    cloudcover INT NOT NULL,
+    cloudcover_low INT NOT NULL,
+    cloudcover_mid INT NOT NULL,
+    cloudcover_high INT NOT NULL,
+    windgusts_10m REAL NOT NULL,
+    shortwave_radiation REAL NOT NULL,
+    direct_radiation REAL NOT NULL,
+    diffuse_radiation REAL NOT NULL,
+    direct_normal_irradiance REAL NOT NULL,
+    et0_fao_evapotranspiration REAL NOT NULL,
+    date DATE NOT NULL,
+    hour TIME NOT NULL
+) ;
+
+CREATE TABLE FLANDERS_WEATHER (
+    temperature_2m REAL NOT NULL,
+    apparent_temperature REAL NOT NULL,
+    dewpoint_2m REAL NOT NULL,  
+    relative_humidity_2m REAL NOT NULL,
+    precipitation REAL NOT NULL,
+    rain REAL NOT NULL,
+    snowfall  REAL NULL,
+    cloudcover INT NOT NULL,
+    cloudcover_low INT NOT NULL,
+    cloudcover_mid INT NOT NULL,
+    cloudcover_high INT NOT NULL,
+    windgusts_10m REAL NOT NULL,
+    shortwave_radiation REAL NOT NULL,
+    direct_radiation REAL NOT NULL,
+    diffuse_radiation REAL NOT NULL,
+    direct_normal_irradiance REAL NOT NULL,
+    et0_fao_evapotranspiration REAL NOT NULL,
+    date DATE NOT NULL,
+    hour TIME NOT NULL
+) ;
+
+CREATE TABLE BRUSSELS_WEATHER (
+    temperature_2m REAL NOT NULL,
+    apparent_temperature REAL NOT NULL,
+    dewpoint_2m REAL NOT NULL,  
+    relative_humidity_2m REAL NOT NULL,
+    precipitation REAL NOT NULL,
+    rain REAL NOT NULL,
+    snowfall  REAL NULL,
+    cloudcover INT NOT NULL,
+    cloudcover_low INT NOT NULL,
+    cloudcover_mid INT NOT NULL,
+    cloudcover_high INT NOT NULL,
+    windgusts_10m REAL NOT NULL,
+    shortwave_radiation REAL NOT NULL,
+    direct_radiation REAL NOT NULL,
+    diffuse_radiation REAL NOT NULL,
+    direct_normal_irradiance REAL NOT NULL,
+    et0_fao_evapotranspiration REAL NOT NULL,
+    date DATE NOT NULL,
+    hour TIME NOT NULL
+) ;
+
+COMMIT;
